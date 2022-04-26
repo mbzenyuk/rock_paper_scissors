d6fb88b26ff0289f0874fcd1482f294e3d013798
Mariya Bzenyuk <mbzenyuk@gmail.com>
2022-04-26 17:12:43 -0500

    start the first function

---
+++ script.js
@@ -0,0 +1,10 @@
+// ** Rock Paper Scissors ** 
+
+//Computer randomly selects 'Rock', 'Paper, 'Scissors'
+
+
+
+//Play one round where the funciton takes the player's selection and the computer's selection and prints "You Lose! Paper beats Rock." Input is not case sensitive.
+
+//Play a new game of 5 rounds, keep the score and report the winner or loser at the end. Use console to print the results of each round.
+
