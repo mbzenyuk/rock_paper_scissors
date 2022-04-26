54cf00a4963338d9ee648c3cc58feba4a0a45d62
Mariya Bzenyuk <mbzenyuk@gmail.com>
2022-04-26 18:43:18 -0500

    add fuction for computer to choose a random string

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 
 
 
+function computerPlay(){
+    let computerArray = ['Rock', 'Paper', 'Scissors'];
+    let computerSelection = computerArray[Math.floor(Math.random()*computerArray.length)];
+    return computerSelection;
+}
+
+console.log(computerPlay())
+
 //Play one round where the funciton takes the player's selection and the computer's selection and prints "You Lose! Paper beats Rock." Input is not case sensitive.
 
 //Play a new game of 5 rounds, keep the score and report the winner or loser at the end. Use console to print the results of each round.
