75dd2559782b28776ba84bfc8af4e8210f15d60c
mbzenyuk <mbzenyuk@gmail.com>
2022-04-28 14:49:43 -0500

    Add all variants of play - doesn't work

--- script.js
+++ script.js
@@ -10,9 +10,34 @@ function computerPlay(){
     return computerSelection;
 }
 
+let playerSelection = prompt('Choose Rock, Paper, Scissors')
 console.log(computerPlay())
+computerSelection = computerPlay()
 
 //Play one round where the funciton takes the player's selection and the computer's selection and prints "You Lose! Paper beats Rock." Input is not case sensitive.
 
-//Play a new game of 5 rounds, keep the score and report the winner or loser at the end. Use console to print the results of each round.
 
+
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === 'rock' && computerSelection === 'Paper'){
+        return `You lose! Paper beats Rock`
+
+    } 
+    
+    if (playerSelection === 'paper' && computerSelection === 'Scissors'){
+        return `You lose! Scissors beats Paper`
+
+    } 
+    
+    if (playerSelection === 'scissors' && computerSelection === 'Rock'){
+        return `You lose! Rock beats scisors`
+    } 
+    
+    else {
+        return `It's a tie! Play again!`
+    }
+
+};
+
+console.log(playRound(playerSelection, computerSelection));
